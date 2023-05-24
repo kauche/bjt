73d9f4d1b066f180aa6b9432d870351d7bfe9b11
Yuki Ito <mrno110y@gmail.com>
2023-05-24 16:38:28 +0900

    doc: fix typo in docs

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-`bjt` (stannds for Base64 JSON Token) is a Go library that provides a simple way to encode and decode tokens using a base64 encoded JSON format.
+`bjt` (stands for Base64 JSON Token) is a Go library that provides a simple way to encode and decode tokens using a base64 encoded JSON format.
 */
 package bjt
